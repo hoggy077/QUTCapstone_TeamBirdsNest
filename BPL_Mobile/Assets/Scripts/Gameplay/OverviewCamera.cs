9901b3c2d6ea2b93c17d9d9f246cc6771697ec99
Lvl20Wizard <tymoncrooks.uni@gmail.com>
2022-08-30 22:38:41 +1000

    Created the bird's eye view camera and enabled left arrow to toggle
    - still need to enable through mobile UI, testing through left arrow on keyboard as temp control
    - also currently having a bug where the Overhead Camera is being activated as the first camera, which fixes itself after swapping to main camera (presumably because until the player presses down the left key, the game switches to a default) - will try fixing tomorrow by adding a boolean

---
+++ BPL_Mobile/Assets/Scripts/Gameplay/OverviewCamera.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverviewCamera : MonoBehaviour
+{
+    public Camera mainCamera, overviewCamera;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("left"))
+        {
+            overHeadCamera();
+        }
+        else if (Input.GetKeyUp("left"))
+        {
+            cameraMain();
+        }
+    }
+
+    public void overHeadCamera()
+    {
+            mainCamera.enabled = false;
+            overviewCamera.enabled = true;
+    }
+
+    public void cameraMain()
+    {
+        mainCamera.enabled = true;
+        overviewCamera.enabled = false;
+    }
+}
